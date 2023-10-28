b5f6fd0b3ad9d97fdae8281919580893b425ac68
Luke Grundvig <lukevig@gmail.com>
2023-10-28 16:14:23 -0500

    Everything but high score working

--- startup-html/gameplay.js
+++ startup-html/gameplay.js
@@ -4,6 +4,9 @@ var startTime;
 var ended = true;
 
 function addScore(){
+    if (ended) {
+        //make some remark about shooting out of turn
+    }
     if (!ended) {
         score++;
     }
@@ -42,7 +45,50 @@ function startGame() {
 }
 
 function endGame() {
+    saveScore(score);
     setTimeout(function() {
+        window.location.reload();
         alert('You shot ' + score + ' time(s)');
-    }, 10);
+    }, 20);
 }
+
+function saveScore(score) {
+
+    const userName = this.getPlayerName();
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+    scores = this.updateScores(userName, score, scores);
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+}
+
+function getPlayerName() {
+    return localStorage.getItem('userName') ?? 'Mystery player';
+}
+
+function updateScores(userName, score, scores) {
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+
+    return scores;
+  }
