2cc6583ec7436a8b3730f8a2156a01cd2e3a151b
Luke Grundvig <lukevig@gmail.com>
2023-11-22 20:23:30 -0700

    changed highscore update system

--- startup-html/public/gameplay.js
+++ startup-html/public/gameplay.js
@@ -69,13 +69,37 @@ function getPlayerName() {
     return localStorage.getItem('userName') ?? 'Mystery player';
 }
 
-function updateScores(userName, score, scores) {
+async function saveScore(score) {
+    const userName = this.getPlayerName();
     const date = new Date().toLocaleDateString();
-    const newScore = { name: userName, score: score, date: date };
+    const newScore = {name: userName, score: score, date: date};
+
+    try {
+      const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(newScore),
+      });
+
+      // Store what the service gave us as the high scores
+      const scores = await response.json();
+      localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+      // If there was an error then just track scores locally
+      this.updateScoresLocal(newScore);
+    }
+}
+
+function updateScoresLocal(newScore) {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
 
     let found = false;
     for (const [i, prevScore] of scores.entries()) {
-      if (score > prevScore.score) {
+      if (newScore > prevScore.score) {
         scores.splice(i, 0, newScore);
         found = true;
         break;
@@ -90,7 +114,7 @@ function updateScores(userName, score, scores) {
       scores.length = 10;
     }
 
-    return scores;
+    localStorage.setItem('scores', JSON.stringify(scores));
 }
 
 function showOtherUserScore() {
