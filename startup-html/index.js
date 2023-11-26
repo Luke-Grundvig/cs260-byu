8f8b98bc15fe4dc01faad18b4624878e46ad10b3
Luke Grundvig <lukevig@gmail.com>
2023-11-26 14:45:09 -0700

    added simon db code, and started db for startup

--- startup-html/index.js
+++ startup-html/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+//curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+//. ~/.nvm/nvm.sh
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
