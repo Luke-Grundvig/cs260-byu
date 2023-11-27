da1a9ad1361ca998b24cf4ea73ae75ff4b3e8fa9
Luke Grundvig <lukevig@gmail.com>
2023-11-26 22:55:00 -0700

    updated to include restrict without login

--- startup-html/public/index.js
+++ startup-html/public/index.js
@@ -1,31 +1,96 @@
-function login() {
-    const nameEl = document.querySelector("#username");
-    const passEl = document.querySelector("#password");
-    localStorage.setItem("password", passEl.value);
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "gameplay.html";
+(async () => {
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
   }
-  
-  function displayQuote(data) {
-    fetch('https://api.quotable.io/random')
-      .then((response) => response.json())
-      .then((data) => {
-        const containerEl = document.querySelector('#quote');
-  
-        const quoteEl = document.createElement('p');
-        quoteEl.classList.add('quote');
-        const authorEl = document.createElement('p');
-        authorEl.classList.add('author');
-  
-        quoteEl.textContent = data.content;
-        authorEl.textContent = data.author;
-  
-        containerEl.appendChild(quoteEl);
-        containerEl.appendChild(authorEl);
-      });
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'gameplay.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+
+function play() {
+  window.location.href = 'gameplay.html';
+}
+
+function logout() {
+  localStorage.removeItem('userName');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
   }
 
-  displayQuote();
+  return null;
+}
 
-  //curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-  //. ~/.nvm/nvm.sh
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
+
+  
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#quote');
+  
+      const quoteEl = document.createElement('p');
+      quoteEl.classList.add('quote');
+      const authorEl = document.createElement('p');
+      authorEl.classList.add('author');
+  
+      quoteEl.textContent = data.content;
+      authorEl.textContent = data.author;
+  
+      containerEl.appendChild(quoteEl);
+      containerEl.appendChild(authorEl);
+    });
+}
+
+displayQuote();
+
+//curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+//. ~/.nvm/nvm.sh
